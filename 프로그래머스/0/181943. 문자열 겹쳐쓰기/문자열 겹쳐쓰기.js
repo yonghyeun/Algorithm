55675a8cb2362962f58bf73b796c63a9ca6e98e1
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-03-06 22:14:32 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,9 +1,13 @@
 function solution(my_string, overwrite_string, s) {
-    const arr = Array.from(my_string)
+    let answer = ''
+    const overLength = overwrite_string.length
     
-    for (let i = 0 ; i < overwrite_string.length ; i += 1){
-        arr[s  + i] = overwrite_string[i]
+    for (let index = 0 ; index < my_string.length ; index += 1){
+        if (index < s || index > s + overLength - 1){
+            answer += my_string[index]
+        } else {
+            answer += overwrite_string[index - s]
+        }
     }
-    
-    return arr.join('')
+    return answer
 }
