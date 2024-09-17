4d4b5a62aaa0bc6a0a059317c50bcfc4942398f9
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-09-17 23:41:32 +0900

    [Bronze II] Title: 수 정렬하기, Time: 156 ms, Memory: 10024 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -7,18 +7,34 @@ const arr = inputs.map((item) => Number(item));
 
 // bubble sort
 
-const bubbleSort = () => {
+// const bubbleSort = () => {
+//   for (let i = 0; i < arr.length - 1; i += 1) {
+//     for (let j = 0; j < arr.length - 1 - i; j += 1) {
+//       if (arr[j] > arr[j + 1]) {
+//         const temp = arr[j + 1];
+//         arr[j + 1] = arr[j];
+//         arr[j] = temp;
+//       }
+//     }
+//   }
+// };
+
+const selectionSort = () => {
   for (let i = 0; i < arr.length - 1; i += 1) {
-    for (let j = 0; j < arr.length - 1 - i; j += 1) {
-      if (arr[j] > arr[j + 1]) {
-        const temp = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = temp;
+    let MIN_INDEX = i;
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[j] < arr[MIN_INDEX]) {
+        MIN_INDEX = j;
       }
+    }
+
+    if (MIN_INDEX !== i) {
+      const temp = arr[MIN_INDEX];
+      arr[MIN_INDEX] = arr[i];
+      arr[i] = temp;
     }
   }
 };
 
-bubbleSort();
-
+selectionSort();
 console.log(arr.join('\n'));
