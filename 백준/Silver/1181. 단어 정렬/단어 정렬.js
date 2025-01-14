f7857064d40a7171cf8a303f0db45543ce55a6de
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-01-14 12:07:00 +0900

    [Silver V] Title: 단어 정렬, Time: 1272 ms, Memory: 25332 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+let [N, ...array] = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const stringMap = array.reduce((map, string) => {
+  const length = string.length;
+  if (map[length]) {
+    map[length][string] = true;
+  } else {
+    map[length] = {
+      [string]: true,
+    };
+  }
+
+  return map;
+}, {});
+
+const insertSort = (array) => {
+  if (array.length <= 1) {
+    return;
+  }
+
+  for (let index = 1; index < array.length; index++) {
+    let prev = index - 1;
+    const item = array[index];
+    while (0 <= prev && item < array[prev]) {
+      array[prev + 1] = array[prev];
+      prev--;
+    }
+
+    if (prev + 1 !== index) {
+      array[prev + 1] = item;
+    }
+  }
+};
+
+const lengthSortedArray = Object.values(stringMap).map(Object.keys);
+lengthSortedArray.forEach((array) => {
+  insertSort(array);
+  array.forEach((string) => {
+    console.log(string);
+  });
+});
