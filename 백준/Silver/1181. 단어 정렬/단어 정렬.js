2b1c6d6083c546d144d1432f72fc807564d1d45e
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-01-14 12:57:46 +0900

    [Silver V] Title: 단어 정렬, Time: 204 ms, Memory: 20024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -3,46 +3,26 @@ const fs = require("fs");
 const filePath =
   process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
 
-let [N, ..._array] = fs.readFileSync(filePath).toString().trim().split("\n");
+let [N, ...inputArray] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
 
-const array = [...new Set(_array)];
-
-const stringMap = array.reduce((map, string) => {
-  const length = string.length;
-  if (map[length]) {
-    map[length][string] = true;
-  } else {
-    map[length] = {
-      [string]: true,
-    };
-  }
-
-  return map;
-}, {});
-
-const insertSort = (array) => {
-  if (array.length <= 1) {
-    return;
-  }
-
-  for (let index = 1; index < array.length; index++) {
-    let prev = index - 1;
-    const item = array[index];
-    while (0 <= prev && item < array[prev]) {
-      array[prev + 1] = array[prev];
-      prev--;
-    }
-
-    if (prev + 1 !== index) {
-      array[prev + 1] = item;
-    }
-  }
-};
-
-const lengthSortedArray = Object.values(stringMap).map(Object.keys);
-const result = lengthSortedArray.flatMap((array) => {
-  insertSort(array);
-  return array;
-});
-
-console.log(result.join("\n"));
+console.log(
+  (() => {
+    const array = [...new Set(inputArray)];
+    array.sort((prev, cur) => {
+      if (prev.length === cur.length) {
+        for (let index = 0; index < prev.length; index++) {
+          if (prev.charCodeAt(index) === cur.charCodeAt(index)) {
+            continue;
+          }
+          return prev.charCodeAt(index) - cur.charCodeAt(index);
+        }
+      }
+      return prev.length - cur.length; 
+    });
+    return array.join("\n");
+  })()
+);
