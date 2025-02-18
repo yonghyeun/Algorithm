a4c82530c1a5f369e726fd9a839645a66c3cfb36
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-02-18 17:22:49 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 156 ms, Memory: 9656 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const filePath =
+  process.platform === "linux"
+    ? "./dev/stdin"
+    : path.join(__dirname, "input.txt");
+
+const N = +require("fs").readFileSync(filePath).toString();
+
+const dp = Array(N + 1).fill(0);
+
+dp[1] = 1;
+dp[2] = 3;
+
+for (let i = 3; i <= N; i++) {
+  const result = (dp[i - 1] % 10007) + ((dp[i - 2] * 2) % 10007);
+  dp[i] = result % 10007;
+}
+
+console.log(dp[N]);
