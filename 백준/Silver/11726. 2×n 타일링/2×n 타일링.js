a9ea07abc6def1760ab241eebfd01d60d786db81
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-02-18 17:11:29 +0900

    [Silver III] Title: 2×n 타일링, Time: 152 ms, Memory: 9648 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -12,7 +12,7 @@ dp[1] = 1;
 dp[2] = 2;
 
 for (let i = 3; i <= N; i++) {
-  const result = (dp[i - 1] % 10007) + (dp[i - 2] % 10007);
+  const result = dp[i-1] + dp[i-2]
   dp[i] = result % 10007;
 }
 
