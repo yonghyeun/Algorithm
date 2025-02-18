52be944fb2b9c8e7df5394a703c58c25b1e7f926
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-02-18 17:37:22 +0900

    [Silver V] Title: 타일 장식물, Time: 100 ms, Memory: 9336 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13301. 타일 장식물/타일 장식물.js
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -1,19 +1,20 @@
+const fs = require("fs");
 const path = require("path");
+
 const filePath =
   process.platform === "linux"
-    ? "./dev/stdin"
+    ? "/dev/stdin"
     : path.join(__dirname, "input.txt");
+const N = BigInt(fs.readFileSync(filePath).toString().trim());
 
-const N = +require("fs").readFileSync(filePath).toString();
+const dp = Array(Number(N) + 1).fill(0n);
 
-const dp = Array(N + 1).fill(0);
+dp[0] = 1n;
+dp[1] = 1n;
 
-dp[0] = 1;
-dp[1] = 1;
-dp[2] = 2;
-
-for (let i = 3; i <= N; i++) {
+for (let i = 2; i <= N; i++) {
   dp[i] = dp[i - 1] + dp[i - 2];
 }
 
-console.log((dp[N] + dp[N - 1]) * 2);
+const perimeter = 2n * (dp[Number(N)] + dp[Number(N) - 1]);
+console.log(perimeter.toString());
