7dd5897aaade0f4c023ee91f5ab94afcfd260bdb
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-03-15 17:12:55 +0900

    [Silver I] Title: 동물원, Time: 156 ms, Memory: 10648 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.js
+++ 백준/Silver/1309. 동물원/동물원.js
@@ -8,19 +8,13 @@ const filePath =
 
 const N = +fs.readFileSync(filePath).toString().trim();
 
-const filled = Array(N).fill(0);
-const empty = Array(N).fill(0);
+const dp = Array(N).fill(0);
 
-filled[0] = 1;
-empty[0] = 1;
+dp[0] = 3;
+dp[1] = 7;
 
-// solution(N) = 2*fill(N) + empty(N)
-// fill(N) = empty(N-1) + fill(N-1)
-// empty(N) = 2*fill(N-1) + empty(N-1)
-
-for (let i = 1; i < N; i++) {
-  filled[i] = (empty[i - 1] + filled[i - 1]) % 9901;
-  empty[i] = (empty[i - 1] + 2 * filled[i - 1]) % 9901;
+for (let i = 2; i < N; i++) {
+  dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901;
 }
 
-console.log((2 * filled[N - 1] + empty[N - 1]) % 9901);
+console.log(dp[N - 1]);
